e87416dca143f3370fafd0adfa44612672bdc63a
imadnanhassan <abulhassan.dev@gmail.com>
2025-04-20 18:21:09 +0600

    feat: replace hardcoded client logos with dynamic data from API

--- src/components/section/HomePage/ClientLogo.tsx
+++ src/components/section/HomePage/ClientLogo.tsx
@@ -1,36 +1,10 @@
 import { HoverEffect } from "@/components/ui/card-hover-effect";
+import { useGetClientLogosQuery } from "@/redux/features/ClientLogo";
 import React from "react";
 
-const projects = [
-  {
-    image:
-      "https://codehaven.io/wp-content/uploads/2025/04/DESIGN-SQUAD-SOURCING-LOGO-WHITE-300x173.png",
-    link: "https://stripe.com",
-   
-  },
-  {
-    image: "https://codehaven.io/wp-content/uploads/2024/05/pcwd-300x98.webp",
-    link: "https://netflix.com",
-  },
-  {
-    image: "https://codehaven.io/wp-content/uploads/2024/03/healthinfobd-1.png",
-    link: "https://google.com",
-  },
-  {
-    image: "https://codehaven.io/wp-content/uploads/2024/03/sam-300x83.png",
-    link: "https://meta.com",
-  },
-  {
-    image: "https://codehaven.io/wp-content/uploads/2024/03/sam-300x83.png",
-    link: "https://amazon.com",
-  },
-  {
-    image:
-      "https://codehaven.io/wp-content/uploads/2025/04/logo-08-300x103.png",
-    link: "https://microsoft.com",
-  },
-];
 const ClientLogo: React.FC = () => {
+  const { data: clientLogo } = useGetClientLogosQuery(undefined);
+  console.log(clientLogo, "clientLogo");
   return (
     <div className="bg-[#0d0d0d]">
       <div className="adnan_container mx-auto  flex flex-col items-center justify-center gap-10 !py-40">
@@ -44,7 +18,7 @@ const ClientLogo: React.FC = () => {
           </h2>
         </div>
         <div className="flex flex-wrap justify-center items-center gap-5">
-          <HoverEffect items={projects} />
+          <HoverEffect items={clientLogo || []} />
         </div>
       </div>
     </div>
